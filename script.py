1fb105b7f492f2c7d2d07d966abdc33aaaf09f4e
emaxximov <maksimov.20.43@gmail.com>
2020-05-31 15:37:15 +0300

    final

--- script.py
+++ script.py
@@ -1,4 +1,15 @@
+########################
+######## IMPORT ########
+########################
+
 import prosodic as p
+import random
+from tkinter import Tk,Frame,Text,Button
+import webbrowser
+
+#########################
+##### syllablegram ######
+#########################
 
 def fsyllablegram(text):
 	scheme=[]
@@ -43,20 +54,196 @@ def fsyllablegram(text):
 	return scheme
 
 p.lang='en'
-# string='from fairest creatures we desire increase \n that thereby beautys rose might never die \n but as the riper should by time decease'
-# string='We will, we will rock you \n We will, we will rock you'
-# string='but as the riper should by time decease'
-# t = p.Text(string)
-t = p.Text('C:\\Users\\HP ProBook\\prosodic_data\\corpora\\corppoetry_en\\test1.txt')
+p.config['print_to_screen']=0
 
-t.parse()
-# list(t.iparse(meter=None,arbiter='Line',line_lim=None,toprint=False))
+########################
+######## DINOIT ########
+########################
 
-text=[]
-for parse in t.bestParses():
-	print(parse)
-	text.append(str(parse))
+yamb1 = ['t-rex', 'minmi', 'troodon']
+yamb2 = ['baryonyx', 'bambiraptor', 'kotasaurus']
+horey1 = ['yinlong']
+horey2 = ['caudipteryx', 'rinchenia', 'triceratops', 'diplodocus']
+dactyl = ['tanius']
+dactyl2 = [['tirex', 'diplodocus'], ['minmi', 'triceratops'], ['troodon', 'caudipteryx']]
+amph1 = ['kaatedocus', 'raptorex', 'zalmoxes', 'aardonyx', 'coelurus']
+anapest1 = ['rapator']
+anapest2 = [['bambiraptor', 'yiulong'], ['baryonyx', 'yinlong']]
 
-syllablegram=fsyllablegram(text)
+def dinoline(line):
+    size = line[0] // 10
+    stress = line[0] % 10
+    parts = (len(line) - 1) // size
+    remainder = len(line) - 1 - parts * size
+    ans = []
+    k = parts
+    while k > 0:
+        if (size == 2):
+            if (stress == 1):
+                if k >= 3:
+                    ans.append(yamb2[random.randint(0,2)])
+                    k -=2
+                else:
+                    ans.append(yamb1[random.randint(0,2)])
+                    k -= 1
+            else:
+                if k >=3:
+                    ans.append(horey2[random.randint(0, 3)])
+                    k -= 2
+                else:
+                    ans.append('yinlong')
+                    k -= 1
+        else:
+            if (stress== 1):
+                if k >= 3:
+                    t = random.randint(0,2)
+                    ans.append(dactyl2[t][0])
+                    ans.append(dactyl2[t][1])
+                    k -= 2
+                else:
+                    ans.append('tanius')
+                    k -= 1
+            elif (stress == 2):
+                ans.append(amph1[random.randint(0,4)])
+                k -= 1
+            else:
+                if k >= 3 and k % 3 == 0:
+                    t = random.randint(0,1)
+                    ans.append(anapest2[t][0])
+                    ans.append(anapest2[t][1])
+                    k -= 2
+                else:
+                    ans.append('rapator')
+                    k -= 1
+    if remainder != 0:
+        if size == 2:
+            if stress == 1:
+                ans.append('khaan')
+            else:
+                ans.pop()
+                ans.append('raptorex')
+        else:
+            if remainder == 2:
+                if stress == 1:
+                    ans.pop()
+                    ans.append('thyreophora')
+                elif stress == 2:
+                    ans.append('yinlong')
+                else:
+                    ans.pop()
+                    ans.append('tsagantegia')
+            else:
+                if stress == 1:
+                    ans.append('khaan')
+                elif stress == 2:
+                    ans.pop()
+                    ans.append('diplodocus')
+                else:
+                    ans.pop()
+                    ans.append('baryonyx')
+    return ans
 
-print(syllablegram)
+
+def dinoit(syllablegram):
+    ans = []
+    for i in range(len(syllablegram)):
+        ans.append([])
+        for word in dinoline(syllablegram[i]):
+            ans[i].append(word)
+    return ans
+
+# text = [[31, 1, 0, 0, 1, 0, 0, 1, 0 , 0, 1, 0], [31, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1]]
+# res = dinoit(text)
+# print(res)
+
+
+#########################
+######## Tkinter ########
+#########################
+
+
+root = Tk()
+
+panel_frame = Frame(root, height = 50, width=50, bg = 'gray')
+text_frame= Frame(root,height=20,width=50)
+panel_frame.pack(side='bottom',fill='both')
+text_frame.pack(side='top')
+
+input_text = Text(text_frame,width=25, height=20, font='Arial 14', wrap='word')
+output_text = Text(text_frame,width=25, height=20, font='Arial 14', wrap='word')
+output_text.insert(1.0,'')
+
+input_text.pack(side = 'left')
+output_text.pack(side = 'right')
+
+go=Button(panel_frame, text = 'Dino it!')
+learn=Button(panel_frame, text = 'Learn with me!')
+
+def go_ans(event):
+	output_text.delete(1.0, 'end')
+	text=input_text.get(1.0, 'end-1c')
+	t = p.Text(text,meter='default_english')
+	t.parse()
+
+	txt=[]
+	for parse in t.bestParses():
+		# print(parse)
+		txt.append(str(parse))
+
+	syllablegram=fsyllablegram(txt)
+	# print(syllablegram)
+
+	to_print=dinoit(syllablegram)
+	# print(to_print)
+	for line in to_print:
+		for word in line:
+			output_text.insert('end',word+' ')
+		output_text.insert('end','\n')
+
+def learn_ans(event):
+	if output_text.tag_ranges('sel'):
+		dino=output_text.get('sel.first', 'sel.last')
+		# print(dino)
+		if(dino=='raptorex'):
+			link='https://en.wikipedia.org/wiki/Raptorex'
+		elif(dino=='kaatedocus'):
+			link='https://en.wikipedia.org/wiki/Kaatedocus'
+		elif(dino=='thyreophora'):
+			link='https://en.wikipedia.org/wiki/Thyreophora'
+		elif(dino=='t-rex'):
+			link='https://www.nhm.ac.uk/discover/dino-directory/tyrannosaurus.html'
+		else:
+			link='https://www.nhm.ac.uk/discover/dino-directory/{}.html'.format(dino)
+		# print(link)
+	else:
+		arr=['https://www.nhm.ac.uk/discover/dino-directory/khaan.html',
+ 'https://www.nhm.ac.uk/discover/dino-directory/minmi.html',
+ 'https://www.nhm.ac.uk/discover/dino-directory/tyrannosaurus.html',
+ 'https://www.nhm.ac.uk/discover/dino-directory/troodon.html',
+ 'https://www.nhm.ac.uk/discover/dino-directory/baryonyx.html',
+ 'https://www.nhm.ac.uk/discover/dino-directory/bambiraptor.html',
+ 'https://www.nhm.ac.uk/discover/dino-directory/kotasaurus.html',
+ 'https://www.nhm.ac.uk/discover/dino-directory/yinlong.html',
+ 'https://www.nhm.ac.uk/discover/dino-directory/caudipteryx.html',
+ 'http://nhm.ac.uk/discover/dino-directory/rinchenia.htmla',
+ 'https://www.nhm.ac.uk/discover/dino-directory/triceratops.html',
+ 'https://www.nhm.ac.uk/discover/dino-directory/diplodocus.html',
+ 'https://www.nhm.ac.uk/discover/dino-directory/tanius.html',
+ 'https://en.wikipedia.org/wiki/Thyreophora',
+ 'https://en.wikipedia.org/wiki/Kaatedocus',
+ 'https://en.wikipedia.org/wiki/Raptorex',
+ 'https://www.nhm.ac.uk/discover/dino-directory/zalmoxes.html',
+ 'https://www.nhm.ac.uk/discover/dino-directory/aardonyx.html',
+ 'https://www.nhm.ac.uk/discover/dino-directory/coelurus.html',
+ 'https://en.wikipedia.org/wiki/Rapator',
+ 'https://www.nhm.ac.uk/discover/dino-directory/tsagantegia.html']
+		link=arr[random.randint(0,len(arr)-1)]
+	webbrowser.open(link, new=2)
+
+go.bind("<Button-1>", go_ans)
+learn.bind("<Button-1>", learn_ans)
+
+go.place(x=100,y=15,width=60,height=20)
+learn.place(x=375,y=15,width=100,height=20)
+
+root.mainloop()
